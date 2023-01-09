3cfa82ca49e6d4a8ce9142defbecc2cd02c17c80
Evelin Arnaudova <evelin.arnaudova@gmail.com>
2023-01-09 20:04:06 +0200

    homework edit

--- homeworks/lectureSeven/Car.java
+++ homeworks/lectureSeven/Car.java
@@ -1,16 +1,13 @@
 package homeworks.lectureSeven;
 
 public class Car {
-    public static void main (String [] args) {
-        Car car = new Car("Audi", "black",true);
-    }
     private String name;
     private String color;
     private int releaseYear;
     private int horsePower;
     private boolean secondHand;
 
-    Car(String name, String color, int releaseYear, int horsePower, boolean secondHand) {
+    public Car(String name, String color, int releaseYear, int horsePower, boolean secondHand) {
         this.name = name;
         this.color = color;
         this.releaseYear = releaseYear;
@@ -19,19 +16,19 @@ public class Car {
 
     }
 
-    Car(String name, String color, int releaseYear, int horsePower) {
+    public Car(String name, String color, int releaseYear, int horsePower) {
         this(name, color, releaseYear, horsePower, false);
     }
 
-    Car(String name, String color, boolean secondHand) {
+    public Car(String name, String color, boolean secondHand) {
         this(name, color,-1, -1, secondHand);
     }
 
-    Car(String name, int releaseYear, int horsePower, boolean secondHand) {
+    public Car(String name, int releaseYear, int horsePower, boolean secondHand) {
         this(name, null, releaseYear, horsePower, secondHand );
     }
 
-    Car(){
+    public Car(){
         System.out.println("No information provided for this car");
     }
 }
---
+++ homeworks/lectureSeven/Exercise.java
@@ -0,0 +1,12 @@
+package homeworks.lectureSeven;
+import homeworks.lectureSeven.Car;
+public class Exercise {
+    public static void main (String [] args) {
+        Car car = new Car("Audi", "blue" ,true);
+        Car car1 = new Car("Volvo", "black", 1992, 600);
+        Car car2 = new Car("Fiat","red",2022,700,true);
+        Car car3 = new Car("VW", 2000,670,true);
+        Car car4 = new Car();
+
+    }
+}
