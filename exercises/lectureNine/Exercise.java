dee2b7def680e88332e8d56a90acd98f4b473a68
Evelin Arnaudova <evelin.arnaudova@gmail.com>
2023-01-16 19:03:42 +0200

    updating lecture nine

--- exercises/lectureNine/Exercise.java
+++ exercises/lectureNine/Exercise.java
@@ -4,10 +4,15 @@ public class Exercise {
     public static void main(String[] args){
 
         Person bulgarian = Person.GetPerson("Ivana", "female", "orthodox", "bulgarian", "QA", "bulgarian", 9204236677l,"USA");
-        Person italian = Person.GetPerson("Martin", "male", "orthodox", "italian","no job", "italian", 9904056677l, "Italia");
+        Person italian = Person.GetPerson("Martin", "male", "orthodox", "italian","no job", "italian", 9104056677l, "Italia");
         Person american = Person.GetPerson("John", "male", "orthodox", "english", "police man", "american", 6708054433l,"USA");
+        Person child = Person.GetPerson("Ivo", "male", "islam", "english", null, "american", 9901057788l, "USA");
 
         ((Bulgarian)bulgarian).celebrateBabaMarta();
+        ((American)american).celebrateThanksgiving();
+        ((Italian)italian).celebrateRegatta();
+        //((Child)child).goToSchool();
+
 
     }
 }
